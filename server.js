e5d10b442629e89c19b76e6774466f112fdc8357
KarthikRam14 <karthikram1441@gmail.com>
2024-02-20 17:55:50 +0530

    connectionSucessfull

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
-const port = 3001;
+require('dotenv').config()
 
-app.get('/',(req, res) => {
-  res.send('Hello, Express!');
+const startDatabase = async() =>{
+  try{
+    await mongoose.connect(process.env.API_LINK)
+    console.log("connected")
+  }catch(err){
+    console.error(err)
+  }
+}
+
+const stopDatabase = async() =>{
+  try{
+    await mongoose.disconnect()
+    console.log("disconnected")
+  }catch(err){
+    console.error(err)
+  }
+}
+
+const isConnected = () => {
+  return mongoose.connection.readyState === 1;
+};
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'o_O',
+    database: isConnected() ? 'connected' : 'disconnected'
+  });
 });
 
-app.listen(port, () => {
-  console.log('port-3001');
+// app.listen(port, () => {
+//   console.log('port-3001');
+// });
+
+const port = process.env.PUBLIC_PORT ?? 3000;
+
+process.on('SIGINT', async () => {
+  await stopDatabase();
+  process.exit(0);
 });
+
+process.on('SIGTERM', async () => {
+  await stopDatabase();
+  process.exit(0);
+});
+
+if (require.main === module) {
+  app.listen(port, async () => {
+    await startDatabase();
+    console.log(`🚀 Server running on PORT: ${port}`);
+  });
+}
