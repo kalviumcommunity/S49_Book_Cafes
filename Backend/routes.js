77a9f19f2a1fe9111a58fec66a58eab4cdb32af5
KarthikRam14 <karthikram1441@gmail.com>
2024-02-22 16:28:24 +0530

    updated routes

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,46 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Mock data for demonstration
 let data = [
-    { id: 1, name: 'BALA', age: 17 },
-    { id: 2, name: 'MAANYA', age: 14 },
-    { id: 3, name: 'ABEL', age: 40 }
+  {
+    id: 1,
+    cafeName: "brewbooks",
+    loc: "Indiranagar",
+    type: "asthetic",
+    ratings: 4.8,
+  },
+  {
+    id: 2,
+    cafeName: "brewbooks2",
+    loc: "Indiranagar2",
+    type: "asthetic2",
+    ratings: 4.8,
+  },
+  {
+    id: 3,
+    cafeName: "brewbooks3",
+    loc: "Indiranagar3",
+    type: "asthetic3",
+    ratings: 4.8,
+  },
 ];
 
 // CRUD operations
-router.post('/create', (req, res) => {
-    const newItem = req.body; // assuming the body contains the new item to be added
-    newItem.id = data.length + 1;
-    data.push(newItem);
-    res.json(newItem);
+router.post("/create", (req, res) => {
+  const newItem = req.body; // assuming the body contains the new item to be added
+  newItem.id = data.length + 1;
+  data.push(newItem);
+  res.json(newItem);
 });
 
-router.get('/read', (req, res) => {
-    res.json(data);
+router.get("/read", (req, res) => {
+  res.json(data);
 });
 
-router.put('/update/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const updateItem = req.body; // assuming the body contains the updated item
-    const index = data.findIndex(item => item.id === id);
-    if (index !== -1) {
-        data[index] = { ...data[index], ...updateItem };
-        res.json(data[index]);
-    } else {
-        res.status(404).json({ message: 'Item not found' });
-    }
+router.put("/update/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updateItem = req.body; // assuming the body contains the updated item
+  const index = data.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    data[index] = { ...data[index], ...updateItem };
+    res.json(data[index]);
+  } else {
+    res.status(404).json({ message: "Item not found" });
+  }
 });
 
-router.delete('/delete/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = data.findIndex(item => item.id === id);
-    if (index !== -1) {
-        const deletedItem = data.splice(index, 1);
-        res.json(deletedItem);
-    } else {
-        res.status(404).json({ message: 'Item not found' });
-    }
+router.delete("/delete/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = data.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    const deletedItem = data.splice(index, 1);
+    res.json(deletedItem);
+  } else {
+    res.status(404).json({ message: "Item not found" });
+  }
 });
 
 module.exports = router;
