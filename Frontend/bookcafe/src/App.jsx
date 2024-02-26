c6de8b46a076aa76f14284ede8b8f379bc253bf6
KarthikRam14 <karthikram1441@gmail.com>
2024-02-26 16:19:27 +0530

    updated with dummy data

--- Frontend/bookcafe/src/App.jsx
+++ Frontend/bookcafe/src/App.jsx
@@ -5,9 +5,6 @@ function App() {
 
   return (
     <>
-
-
-    
       <Home/>
     </>
   )
--- Frontend/bookcafe/src/components/Home.jsx
+++ Frontend/bookcafe/src/components/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import dummydata from './dummydata.json'
 
 function Home() {
+
+  
+
   return (
     <div>
-        <div>Landing page</div>
+        <div>{dummydata.map((item)=>{
+        return <div>
+          <h1>{item.id}</h1>
+          <h1>{item.cafeName}</h1>
+          <h3>{item.loc}</h3>
+          <h4>{item.type}</h4>
+          <h4>{item.ratings}</h4>
+          </div>
+        })}</div>
     </div>
   )
 }
